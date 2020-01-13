c6cea80e606ae148af976b0f3967f32653e1822d
matbot <matbot@pop-os.localdomain>
2020-01-13 14:45:41 -0800

    initial commit: stagehand console-full

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:SpaceAdventure/SpaceAdventure.dart' as SpaceAdventure;
+
+void main(List<String> arguments) {
+  print('Hello world: ${SpaceAdventure.calculate()}!');
+}
---
+++ test/SpaceAdventure_test.dart
@@ -0,0 +1,8 @@
+import 'package:SpaceAdventure/SpaceAdventure.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
