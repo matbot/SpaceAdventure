d66cee950b9bc4b2b3e94eed748df8f0af28f288
matbot <matbot@pop-os.localdomain>
2020-01-13 20:18:20 -0800

    started conditional flow

--- bin/main.dart
+++ bin/main.dart
@@ -8,18 +8,28 @@ void main(List<String> arguments) {
 
   print("What's your name?");
 
-  String name = stdin.readLineSync();
+  final name = stdin.readLineSync();
+  // const foo = 42; // just an example of a compile time constant in dart.
 
-  print("Nice to meet you, $name. My name is Eliza. I'm an old friend of Alexa.");
+  print(
+      "Nice to meet you, $name. My name is Eliza. I'm an old friend of Alexa.");
 
   print("Let's go on an adventure!\n"
       "Shall I randomly choose a planet for you to visit? (Y or N)");
 
-  String answer = stdin.readLineSync();
+  String answer;
+  while (answer != 'Y' && answer != 'N') {
+    answer = stdin.readLineSync();
+    print("Sorry, I didn't get that.");
+  }
 
-  print("Name the planet you would like to visit.");
-
-  String planet = stdin.readLineSync();
+  String planet;
+  if (answer == 'Y') {
+    planet = "Neptune";
+  } else if (answer == 'N') {
+    print("Name the planet you would like to visit.");
+    planet = stdin.readLineSync();
+  }
 
   print("Traveling to $planet...\n"
       "Arrived at $planet. A very cold planet, furthest from the sun.");
