bfddd7ef88f988d1271052cd4d8984b48f0f57a3
matbot <matbot@pop-os.localdomain>
2020-01-19 06:39:41 -0800

    simplified main to a neat one liner, added commentary

--- bin/main.dart
+++ bin/main.dart
@@ -8,12 +8,16 @@ import 'dart:convert';
 import 'package:SpaceAdventure/space_adventure.dart';
 
 void main(List<String> arguments) {
-  Map<String, dynamic> systemMap;
   // Concise file consumption, inline class object instantiation, and member function call.
-  // Kind of hard to read, but pretty neat.
-  File(arguments[0]).readAsString().then((String contents) {
-    SpaceAdventure(
-            planetarySystem: PlanetarySystem.fromJson(jsonDecode(contents)))
-        .start();
-  });
+  // Kind of hard to read, but pretty neat as an example of a powerful one liner in dart.
+  // 1. Open the file named by the first argument.
+  // 2. Read the contents of that file as a string.
+  // 3. Then, pass the string contents to an anonymous function,
+  // 4. which initializes an SpaceAdventure class object using the contents,
+  // 5. which initializes a PlanetarySystem object,
+  // 6. which initializes the Planet objects.
+  // 7. Finally, the SpaceAdventure objects start() method is called.
+  File(arguments[0]).readAsString().then((String contents) => SpaceAdventure(
+          planetarySystem: PlanetarySystem.fromJson(jsonDecode(contents)))
+      .start());
 }
